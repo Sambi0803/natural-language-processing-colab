6ff32c98c31e3d3cbe0e653e1506d0b74a7df94c
Sambi0803 <50853561+Sambi0803@users.noreply.github.com>
2019-09-24 17:30:41 -0500

    Update utils.py

--- project/utils.py
+++ project/utils.py
@@ -18,7 +18,7 @@ RESOURCE_PATH = {
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
-
+    
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
     bad_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
@@ -33,15 +33,13 @@ def text_prepare(text):
 
 def load_embeddings(embeddings_path):
     """Loads pre-trained word embeddings from tsv file.
-
     Args:
       embeddings_path - path to the embeddings file.
-
     Returns:
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
     """
-
+    
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
@@ -49,28 +47,35 @@ def load_embeddings(embeddings_path):
     ########################
     #### YOUR CODE HERE ####
     ########################
-
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/AVBelyy/natural-language-processing-colab/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    
+    embeddings = dict()
+    for line in open(embeddings_path, encoding='utf-8'):
+        row = line.strip().split('\t')
+        embeddings[row[0]] = np.array(row[1:], dtype=np.float32)
+    embeddings_dim = embeddings[list(embeddings)[0]].shape[0]
+    
+    return embeddings, embeddings_dim
+    
 
 
 def question_to_vec(question, embeddings, dim):
     """Transforms a string to an embedding by averaging word embeddings."""
-
+    
     # Hint: you have already implemented exactly this function in the 3rd assignment.
 
     ########################
     #### YOUR CODE HERE ####
     ########################
-
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/AVBelyy/natural-language-processing-colab/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    result = np.zeros(dim)
+    cnt = 0
+    words = question.split()
+    for word in words:
+        if word in embeddings:
+            result += np.array(embeddings[word])
+            cnt += 1
+    if cnt != 0:
+        result /= cnt
+    return result 
 
 
 def unpickle_file(filename):
